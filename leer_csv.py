734a41f8d73c5295fd5ad8ef33db6cad256d5bf4
nuevo_63 <ajrahuacso@gmail.com>
2024-07-09 14:27:50 -0500

    Correción para la lectura del archivo

--- leer_csv.py
+++ leer_csv.py
@@ -1,18 +1,17 @@
-from cancion import Cancion  # Asumiendo que Cancion está definida en cancion.py
+from cancion import Cancion
 import csv
 
 def leer_csv(archivo_csv):
     canciones = []
     with open(archivo_csv, newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader, None) 
+        reader = csv.DictReader(csvfile)
 
         for row in reader:
             if len(row) >= 17: 
                 cancion = Cancion()
                 cancion.set_artist_name(row['artist_name'].strip())
                 cancion.set_track_name(row['track_name'].strip())
-                cancion.set_track_id(int(row['track_id']))
+                cancion.set_track_id(row['track_id'].strip()) 
                 cancion.set_popularity(int(row['popularity']))
                 cancion.set_year(int(row['year']))
                 cancion.set_duration_ms(int(row['duration_ms']))
