b03dd4018cd8d5fa3a82dffe97faf17f65c070fd
nuevo_63 <ajrahuacso@gmail.com>
2024-07-09 00:39:46 -0500

    Estructura general y cambio de la clase Cancion con otras propiedades

--- Main.py
+++ Main.py
@@ -1,22 +1,5 @@
+from cancion import Cancion
 import csv
-
-#Clase canción para los Objetos
-class Cancion:
-    def __init__(self):
-        self.artist_name = ""
-        self.track_name = ""
-
-    def set_artist_name(self, name):
-        self.artist_name = name
-
-    def set_track_name(self, name):
-        self.track_name = name
-
-    def get_artist_name(self):
-        return self.artist_name
-
-    def get_track_name(self):
-        return self.track_name
 
 #Método para leer el archivo
 def leer_csv(archivo_csv):
@@ -32,14 +15,15 @@ def leer_csv(archivo_csv):
                 canciones.append(cancion)
     return canciones
 
+#Main para la ejecucipon de prueba
 def main():
     archivo_csv = "./archive/spotify_data.csv"
     try:
         canciones = leer_csv(archivo_csv)
-        for cancion in canciones:
+        for cancion in canciones[:10]:
             print(f"Nombre de Artista: {cancion.get_artist_name()} y Canción del Artista: {cancion.get_track_name()}")
     except IOError as e:
         print(f"Error leyendo el archivo: {e}")
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Se asegura ejecutar de manera independiente
     main()
---
+++ cancion.py
@@ -0,0 +1,37 @@
+class Cancion:
+    def __init__(self):
+        self.artist_name = ""
+        self.track_name = ""
+        self.track_year = ""
+        self.track_popularity = 0  
+        self.track_duration_ms = 0  
+
+    def set_artist_name(self, name):
+        self.artist_name = name
+
+    def set_track_name(self, name):
+        self.track_name = name
+
+    def set_year(self, year):
+        self.track_year = year
+
+    def set_popularity(self, popularity):
+        self.track_popularity = popularity
+
+    def set_duration_ms(self, duration):
+        self.track_duration_ms = duration
+
+    def get_artist_name(self):
+        return self.artist_name
+
+    def get_track_name(self):
+        return self.track_name
+    
+    def get_year(self):
+        return self.track_year
+    
+    def get_popularity(self):
+        return self.track_popularity
+
+    def get_duration_ms(self):
+        return self.track_duration_ms
